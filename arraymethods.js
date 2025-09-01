a1d4a3b61ac8221a28f74754c996990707100ffd
Himanshu <himanshugautam5309@gmail.com>
2025-09-01 17:27:28 +0530

    Add files via upload

---
+++ arraymethods.js
@@ -0,0 +1,53 @@
+// let array = [1,2,3,4,5,6]
+// let string = array.toString()
+// console.log(array);
+// console.log(string);
+// let newArray = array.join("-");
+// console.log(newArray);
+// array.pop();
+// console.log(array);
+// array.push(10);
+// console.log(array);
+// let array2 = [2,3,2,1,1]
+// let newArray2 = array.concat(array2);
+// console.log(newArray2);
+// let arrayNew = ["chair", "almirah","wooden doors", "table"];
+// arrayNew.shift();
+// console.log(arrayNew);
+// arrayNew.unshift("sandook");
+// console.log(arrayNew);
+
+// splice method
+
+// arrayNew.splice(1,2,"sofa","pillow");
+// console.log(arrayNew);
+// console.log(arrayNew.length);
+// slice method
+// let furniture = arrayNew.slice(1,3);
+// console.log(furniture);
+// arrayNew.reverse();
+// console.log(arrayNew);
+// sort method
+// arrayNew.sort();
+// console.log(arrayNew);
+
+// map method 
+
+// const array = ['4','9','25','64'];
+// const newArray = array.map((x) => {
+//     return Math.sqrt(x);
+// });
+
+// console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
